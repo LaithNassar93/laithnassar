9af3853e53e52a4d0fa4ed4544e511dcc5c12d51
laithnassar93 <laith.nassar93@gmail.com>
2022-06-26 20:23:15 -0500

    main

--- lab5.js
+++ lab5.js
@@ -1,11 +1,10 @@
-console.log(sum([1, 50, 80, 80, 2]));
+console.log(sum([3, 12, 50, 90, 22]));
 console.log(getNewArray(['asdsads', 'aaaasd', 'HHHAAA', 'Laith', 'Nassar']));
 
 function sum(arr) {
-    return arr.filter(elem => elem > 20).reduce((accu, current) => accu + current, 0);
+    return arr.filter(temp => temp > 20).reduce((accu, current) => accu + current, 0);
 }
 
 const getNewArray = function(arr) {
-    // return arr.filter(elem => elem.includes('a') && elem.length >= 5);
-    return arr.filter(elem => elem.includes('a')).filter(elem => elem.length >= 5);
+    return arr.filter(temp => temp.includes('a')).filter(temp => temp.length >= 5);
 }
